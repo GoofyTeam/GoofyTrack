9499227fe148bdff01fa1e5d2a1ea08a9a9eca48
evoaga <qirahakan@gmail.com>
2025-05-16 10:07:12 +0200

    feat: planning table

--- app/components/talks/TalksSchedule.tsx
+++ app/components/talks/TalksSchedule.tsx
@@ -21,6 +21,7 @@ import { cn } from '@/lib/utils';
 import { isOrganizer } from '@/utils/auth.utils';
 import type { Talk } from '@/lib/types';
 import PlanningOverview from './PlanningOverview';
+import PlanningTable from './PlanningOverview';
 
 //
 // Define the shape coming back from /api/rooms/availability
@@ -288,10 +289,7 @@ export default function TalksSchedule({ talks, onScheduleTalk }: TalksSchedulePr
           </CardContent>
         </Card>
       </div>
-      <PlanningOverview
-        rooms={rooms.map(({ roomId, name }) => ({ roomId, name }))}
-        date={selectedDate}
-      />
+      <PlanningTable />
     </div>
   );
 }
